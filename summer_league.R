f88bc8639bf58cf87bd9abf3a1dbfb5c93ccd399
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2026-07-04 19:42:06 +0200

    Refactor summer_league.R to clean up code and paths
    Removed unused libraries and code related to social media links. Added functionality to ensure the data output folder exists and updated the CSV output path.

--- summer_league.R
+++ summer_league.R
@@ -1,23 +1,15 @@
 library(dplyr)
-library(hoopR)
 library(janitor)
 library(hablar)
-library(gt)
-library(gtExtras)
-library(gtUtils)
 library(httr)
 library(jsonlite)
 
+# get today's date
+today <- Sys.Date() 
 
-twitter <- "<span style='color:#b14f04'>&#x1D54F;</span>"
-tweetelcheff <- "<span style='font-weight:bold;color: grey;'>*@elcheff*</span>"
-insta <- "<span style='color:#E1306C;font-family: \"Font Awesome 6 Brands\"'>&#xE055;</span>"
-instaelcheff <- "<span style='font-weight:bold;color: grey;'>*@sport_iv0*</span>"
-github <- "<span style='color:#c8102e;font-family: \"Font Awesome 6 Brands\"'>&#xF092;</span>"
-githubelcheff <- "<span style='font-weight:bold;color: grey;'>*IvoVillanueva*</span>"
-caption <- glue::glue("**Datos**: *@NBA* | **Gráfico**: *Ivo Villanueva* • {twitter} {tweetelcheff} • The Clean Shot")
-
-
+# ensure data output folder exists
+if (!dir.exists("data")) dir.create("data")
+ 
 headers <- c(
   "accept" = "*/*",
   "accept-encoding" = "gzip, deflate, br, zstd",
@@ -43,86 +35,8 @@ res <- GET(url = url, add_headers(.headers=headers))
 json_resp <- fromJSON(content(res, "text"))
 
 
-df <- as_tibble(json_resp$resultSets$rowSet[[1]],
+as_tibble(json_resp$resultSets$rowSet[[1]],
                 .name_repair = ~json_resp$resultSets$headers[[1]]) %>%
 retype() %>%
   clean_names() %>%
-   transmute(logo = ifelse(team_abbreviation == "GWG", "GSW",team_abbreviation ),
-          logo = paste0("https://raw.githubusercontent.com/IvoVillanueva/NBA/refs/heads/main/logos_cuadrados/",logo, ".png"),
-          player_name,
-          min,
-          pts,
-          reb,
-          ast,
-          tov,
-          stl,
-          blk,
-          pf,
-          fgm,
-          fga,
-          fg3m,
-          fg3a,
-          ftm,
-          fta,
-          plus_minus,
-          dre = (.79*pts-.72*(fga-fg3a)-.55*fg3a-.16*fta+.13*oreb+.40*dreb+.54*ast+1.68*stl+.76*blk-1.36*tov-.11*pf)
-   ) %>%
-   mutate(fgm = paste0(fgm, "/", fga),
-          fg3m = paste0(fg3m, "/", fg3a),
-          ftm = paste0(ftm, "/", fta)) %>%
-   arrange(desc(dre)) %>%
-   # take top 20
-   filter(row_number() <= 20) %>%
-  write.csv(paste0("data/mvp_odds_", gsub("-", "_", today), ".csv"), row.names = F)
-
- p %>%
-   gt() %>%
-   tab_header(title = md("**Best of Summer League Day One**"),
-              subtitle = "Los 20 mejores en RAPM Diario Estimado (DRE) en el primer día de la Summer League de Las Vegas"
-   ) %>%
-   tab_source_note(
-     source_note = md(caption)
-   ) %>%
-   cols_label(logo = "",
-              player_name = "Player",
-              min = "MIN",
-              pts = "PTS",
-              reb = "REB",
-              ast = "AST",
-              tov = "TOV",
-              stl = "STL",
-              blk = "BLK",
-              fgm = "FGM/A",
-              fg3m = "3PM/A",
-              ftm = "FTM/A",
-              plus_minus = "+/-",
-              dre = "DRE") %>%
-   fmt_number(plus_minus, force_sign = T, decimals = 0) %>%
-   fmt_number(dre, decimals = 1) %>%
-   gt_img_rows(columns = logo) %>%
-   data_color(columns = "dre",
-              alpha = .75,
-              reverse = F,
-              palette = "ggsci::blue_grey_material") %>%
-   tab_options(data_row.padding = '0px',
-               table.font.names = "Bebas Neue",
-               table_body.hlines.color = "transparent",
-               column_labels.border.top.color = 'black',
-               column_labels.border.top.width = px(1),
-               column_labels.border.bottom.style = 'none',
-               column_labels.font.weight = "strong",
-               row_group.border.top.style = "none",
-               row_group.border.top.color = "black",
-               row_group.border.bottom.width = px(1),
-               row_group.border.bottom.color = "black",
-               row_group.border.bottom.style = "solid",
-               row_group.padding = px(1.5),
-               heading.align = 'left',
-               heading.border.bottom.style = "none",
-               table_body.border.top.style = "none",
-               table_body.border.bottom.color = "white",
-               table.border.bottom.style = 'none',
-               table.border.top.style = 'none',
-               source_notes.border.lr.style = "none")%>%
-   # save table
-   gt_save_crop("summer_league_r_tbl.png")
+  write.csv(paste0("data/summer_2026", gsub("-", "_", today), ".csv"), row.names = F)
